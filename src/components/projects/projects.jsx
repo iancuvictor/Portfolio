f951a8650e86a0472be6e8ce2a82b1d597e62dbf
Iancu Victor - Iuliu <iancu27victor@gmail.com>
2022-06-19 16:53:07 +0300

    Modified projects tab (changed one of the cards and added github links to the projects)

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import { ImHtmlFive } from 'react-icons/im';
 import { IoLogoCss3 } from 'react-icons/io';
-import { SiJavascript } from 'react-icons/si';
+import { SiJavascript, SiNextdotjs } from 'react-icons/si';
 import { FaReact } from 'react-icons/fa';
 import css from './projects.module.css';
 import { ProjectSlide } from '../index';
-import ukraine from './assets/ukraine.png';
+import UAEEstate from './assets/UAEEstate.png';
 import portfolio from './assets/portfolio.png';
 
 
 
-function Projects(props) {
+function Projects() {
     return (
         <div id='projectsPage' className={css.projectsPage}>
+            <div className={css.background}></div>
             <div className={css.content}>
                 <h1>Projects</h1>
                 <div className={css.projectsList}>
-                    <ProjectSlide image={ukraine} title={'The Ukrainian War'} description={'This is a project I started making at the beginning of the Russo-Ukrainian war. It is not updated live, and it wasn\'t meant to be. This project will be finished at the end of this conflict.'} madeIn={<><FaReact size={25} /><ImHtmlFive size={25} /><IoLogoCss3 size={25} /><SiJavascript size={25} /></>} githubLink={'https://github.com/iancuvictor'}/>
-                    <ProjectSlide image={portfolio} title={'This Portfolio Website'} description={'Well... if you want to see the source code of this website, there you go:'} madeIn={<><FaReact size={25} /><ImHtmlFive size={25} /><IoLogoCss3 size={25} /><SiJavascript size={25} /></>} githubLink={'https://github.com/iancuvictor'}/>
+                    <ProjectSlide 
+                        image={UAEEstate} 
+                        title={'United Arab Emirates Real Estate'} 
+                        description={`This project showcases my Frontend skills (the backend being 90% copied from another source). 
+                                    Note that I do NOT know Next.js, so Frontend aside, this project is pretty much a copy.`} 
+                        madeIn={<><SiNextdotjs size={25} /><ImHtmlFive size={25} /><IoLogoCss3 size={25} /><SiJavascript size={25} /></>} 
+                        githubLink={'https://github.com/iancuvictor/Real-Estate-Proiect-Deployment'}/>
+                    <ProjectSlide 
+                        image={portfolio} 
+                        title={'This Portfolio Website'} 
+                        description={'Well... if you want to see the source code of this website, there you go:'} 
+                        madeIn={<><FaReact size={25} /><ImHtmlFive size={25} /><IoLogoCss3 size={25} /><SiJavascript size={25} /></>} 
+                        githubLink={'https://github.com/iancuvictor/Portfolio'}/>
                 </div>
             </div>
-            <div className={css.background}></div>
         </div>
     );
 };
